0555a71250ad74041e1c1a5abc8273e33ae552ce
WebmasterOmnimedia <webmaster@grupo-omnimedia.com>
2021-10-15 09:17:40 +0200

    Meto el menu de navegación y la imagen top

--- src/Controller/TemasController.php
+++ src/Controller/TemasController.php
@@ -5,16 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Cursos;
+use App\Entity\Centros;
+use App\Entity\Asignaturas;
+use App\Entity\Temas;
 
 class TemasController extends AbstractController
 {
-    /**
-     * @Route("/temas", name="temas")
-     */
-    public function index(): Response
-    {
-        return $this->render('temas/index.html.twig', [
-            'controller_name' => 'TemasController',
-        ]);
-    }
+  /**
+   * @Route("{centro}/{curso}/{asignatura}/{tema}", name="tema")
+   */
+  public function index($centro, $curso, $asignatura, $tema): Response
+  {
+    $repo_centros = $this->getDoctrine()->getRepository(Centros::class);
+    $paso_centro = $repo_centros->findOneBy(['slug' => $centro]);
+
+    $repo_cursos = $this->getDoctrine()->getRepository(Cursos::class);
+    $paso_curso = $repo_cursos->findOneBy(['slug' => $curso]);
+
+    $repo_asignaturas = $this->getDoctrine()->getRepository(Asignaturas::class);
+    $paso_asignatura = $repo_asignaturas->findOneBy(['slug' => $asignatura]);
+
+    $repo_temas = $this->getDoctrine()->getRepository(Temas::class);
+    $paso_tema = $repo_temas->findOneBy(['slug' => $tema]);
+
+      return $this->render('temas/index.html.twig', [
+          'controller_name' => 'TemasController',
+          'centro' => $paso_centro,
+          'curso' => $paso_curso,
+          'asignatura' => $paso_asignatura,
+          'tema' => $paso_tema
+      ]);
+  }
 }
